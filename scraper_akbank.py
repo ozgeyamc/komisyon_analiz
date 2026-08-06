2798548b772878d6ee62ecddf67dc2b931f40f50
ozgeyamc <ozgee.yamacc@gmail.com>
2026-08-06 17:55:00 +0300

    Update scraper_akbank.py

--- scraper_akbank.py
+++ scraper_akbank.py
@@ -11,12 +11,12 @@ from typing import List
 AKBANK_URL = "https://www.akbank.com/urun-ve-hizmet-ucretleri"
 
 DATE_PATTERN = re.compile(
-    r"G[üu]ncell[ei]nme\s*Tarihi\s*:\s*(\d{2}\.\d{2}\.\d{4}(?:\s+\d{2}:\d{2})?)",
+    r"G[üu]ncellenme\s*Tarihi\s*:\s*(\d{2}\.\d{2}\.\d{4}(?:\s+\d{2}:\d{2})?)",
     re.IGNORECASE,
 )
 
 DATE_PATTERN_TR = re.compile(
-    r"(?:son\s+)?g[üu]ncell[ei]nme\s+tarihi\s*:?\s*"
+    r"(?:son\s+)?g[üu]ncellenme\s+tarihi\s*:?\s*"
     r"(\d{1,2})\s+(ocak|şubat|mart|nisan|mayıs|haziran|temmuz|ağustos|eylül|ekim|kasım|aralık)\s+(\d{4})",
     re.IGNORECASE,
 )
